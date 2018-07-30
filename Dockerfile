46e46dc4c0ddbf000438391e7578e3b0134a63d4
邱亚涛 <qiuyatao@117sport.com>
2018-07-30 10:39:02 +0800

    docker hive metastore

--- Dockerfile
+++ Dockerfile
@@ -6,20 +6,26 @@ USER root
 
 ADD cloudera-cdh5.repo /etc/yum.repos.d/
 RUN rpm --import https://archive.cloudera.com/cdh5/redhat/5/x86_64/cdh/RPM-GPG-KEY-cloudera
-RUN yum install -y postgresql hive hive-hbase hive-jdbc hive-metastore hive-server2
+#RUN yum install -y postgresql hive hive-hbase hive-jdbc hive-metastore hive-server2
+RUN yum install postgresql-jdbc hive-metastore -y
 RUN yum clean all
+RUN yum install mysql-connector-java -y
+RUN yum install postgresql -y
 
-RUN wget https://jdbc.postgresql.org/download/postgresql-9.4.1209.jre7.jar -O /usr/lib/hive/lib/postgresql-9.4.1209.jre7.jar
+RUN ln -s /usr/share/java/postgresql-jdbc.jar /usr/lib/hive/lib/postgresql-jdbc.jar
+RUN ln -s /usr/share/java/mysql-connector-java.jar /usr/lib/hive/lib/mysql-connector-java.jar
+#RUN wget https://jdbc.postgresql.org/download/postgresql-9.4.1209.jre7.jar -O /usr/lib/hive/lib/postgresql-9.4.1209.jre7.jar
 
 WORKDIR /
 
-ADD etc/supervisord.conf /etc/
-ADD etc/hive/conf/*.xml /etc/hive/conf/
-ADD etc/hadoop/conf/*.xml /etc/hadoop/conf/
+ADD etc/default/supervisord.conf /etc/
+ADD etc/default/hive/conf/*.xml /etc/hive/conf/
+ADD etc/default/hadoop/conf/*.xml /etc/hadoop/conf/
 ADD bin/supervisord-bootstrap.sh ./
 ADD bin/wait-for-it.sh ./
 RUN chmod +x ./*.sh
 
-EXPOSE 9083 10000 10002
+#EXPOSE 9083 10000 10002
+EXPOSE 9083
 
 ENTRYPOINT ["supervisord", "-c", "/etc/supervisord.conf", "-n"]
--- bin/supervisord-bootstrap.sh
+++ bin/supervisord-bootstrap.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+DB_HOST=`cat /etc/hive/conf/hive-site.xml  | grep jdbc: | awk -F ":|/" '{print $5}'`
+DB_PORT=`cat /etc/hive/conf/hive-site.xml  | grep jdbc: | awk -F ":|/" '{print $6}'`
+DB_USER=`cat /etc/hive/conf/hive-site.xml  | grep -A 1 ConnectionUserName | grep value | awk -F">|<" '{print $3}'`
+DB_PASSWORD=`cat /etc/hive/conf/hive-site.xml  | grep -A 1 ConnectionPassword | grep value | awk -F">|<" '{print $3}'`
+
+echo "db config: ${DB_TYPE} => ${DB_USER}@${DB_HOST}:${DB_PORT}"
 
 rm /opt/hive/logs/*.pid 2> /dev/null
 
@@ -20,14 +26,22 @@ if [ $rc -ne 0 ]; then
     exit $rc
 fi
 
-/wait-for-it.sh postgres:5432 -t 120
+/wait-for-it.sh $DB_HOST:$DB_PORT -t 120
 rc=$?
 if [ $rc -ne 0 ]; then
     echo -e "\n--------------------------------------------"
-    echo -e "    PostgreSql not ready! Exiting..."
+    echo -e "    DB:$DB_TYPE not ready! Exiting..."
     echo -e "--------------------------------------------"
     exit $rc
 fi
+#/wait-for-it.sh postgres:5432 -t 120
+#rc=$?
+#if [ $rc -ne 0 ]; then
+#    echo -e "\n--------------------------------------------"
+#    echo -e "    PostgreSql not ready! Exiting..."
+#    echo -e "--------------------------------------------"
+#    exit $rc
+#fi
 
 #sudo -u hdfs hdfs dfs -mkdir -p /tmp
 #sudo -u hdfs hdfs dfs -mkdir -p /user/
@@ -38,9 +52,11 @@ fi
 #sudo -u hdfs hdfs dfs -chown -R hdfs:supergroup /tmp
 #sudo -u hdfs hdfs dfs -chown -R hdfs:supergroup /user
 
-psql -h postgres -U postgres -c "CREATE DATABASE metastore;" 2>/dev/null
+# need create metastore manually
+#psql -h postgres -U postgres -c "CREATE DATABASE metastore;" 2>/dev/null
 
-/usr/lib/hive/bin/schematool -dbType postgres -initSchema
+#/usr/lib/hive/bin/schematool -dbType postgres -initSchema
+/usr/lib/hive/bin/schematool -dbType $DB_TYPE -initSchema
 
 supervisorctl start hive-metastore
 
@@ -53,19 +69,19 @@ if [ $rc -ne 0 ]; then
     exit $rc
 fi
 
-supervisorctl start hive-server2
-/wait-for-it.sh localhost:10002 -t 240
-rc=$?
-if [ $rc -ne 0 ]; then
-    echo -e "\n---------------------------------------"
-    echo -e "   HiveServer2 not ready! Exiting..."
-    echo -e "---------------------------------------"
-    exit $rc
-fi
-
-echo -e "\n\n--------------------------------------------------------------------------------"
-echo -e "You can now access to the following Hive Web UIs:"
-echo -e ""
-echo -e "HiveServer2 Web Interface:		http://localhost:10002"
-echo -e "\nMantainer:   Matteo Capitanio <matteo.capitanio@gmail.com>"
-echo -e "--------------------------------------------------------------------------------\n\n"
+#supervisorctl start hive-server2
+#/wait-for-it.sh localhost:10002 -t 240
+#rc=$?
+#if [ $rc -ne 0 ]; then
+#    echo -e "\n---------------------------------------"
+#    echo -e "   HiveServer2 not ready! Exiting..."
+#    echo -e "---------------------------------------"
+#    exit $rc
+#fi
+#
+#echo -e "\n\n--------------------------------------------------------------------------------"
+#echo -e "You can now access to the following Hive Web UIs:"
+#echo -e ""
+#echo -e "HiveServer2 Web Interface:		http://localhost:10002"
+#echo -e "\nMantainer:   Matteo Capitanio <matteo.capitanio@gmail.com>"
+#echo -e "--------------------------------------------------------------------------------\n\n"
